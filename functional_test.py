f331915e87041277fe170125d39038b116c92d53
erichht <erichht71@gmail.com>
2019-01-21 18:54:32 +1300

    Refactor home page view to use a template

--- functional_test.py
+++ functional_test.py
@@ -5,8 +5,8 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
     
+        self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
     
