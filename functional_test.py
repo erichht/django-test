96e4981f9fcd8308803fc05b85e09ae68d2d8c53
erichht <erichht71@gmail.com>
2019-01-23 10:21:06 +1300

    Model for list Items and associated migration

--- functional_test.py
+++ functional_test.py
@@ -9,6 +9,11 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn(row_text, [row_text for row in rows])
     
     def test_can_start_a_list_and_retrieve_it_late(self):
 
