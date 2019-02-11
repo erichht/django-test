881ffe7dbdbf1c83fc1ceee2e8c0cd466c00deff
erichht <erichht71@gmail.com>
2019-02-12 10:43:02 +1300

    Added Item and List dependent relationship. Same URL assertion between francis_list_url and edith_list_url still exists though

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -8,7 +8,8 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
     
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        list_ = List.objects.create()
+        response = self.client.post('/', data={'item_text': 'A new list item', list: list_})
         self.assertEqual(response.status_code,  302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
@@ -16,15 +17,23 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
     
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -32,7 +41,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -40,8 +51,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
