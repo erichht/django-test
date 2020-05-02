33d01271204a43120800c89dba8f7d7299f2d4a3
erichht <erichht71@gmail.com>
2020-05-03 11:58:10 +1200

    test fabfile.py step by step

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,44 +1,50 @@
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
 import random
-from fabric.contrib.files import append, exists
-from fabric.api import cd, env, local, run
 
 REPO_URL = 'https://github.com/erichht/django-test.git'
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}'
-    run(f'mkdir -p {site_folder}')
-    with cd(site_folder):
-        _get_latest_source()
-        _update_virtualenv()
-        _create_or_update_dotenv()
-        _update_static_files()
-        _update_database()
+    site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_source(source_folder)
+    _update_setting(source_folder, env.host)
+    # _update_virtualenv(source_folder)
+    # _update_static_files(source_folder)
+    # _update_database(source_folder)
 
-def _get_latest_source():
-    if exists('.git'):
-        run('git fetch')
+def _create_directory_structure_if_necessary(site_folder):
+    for subfolder in ('database', 'static', 'virtualenv', 'source'):
+        run('mkdir -p %s/%s' % (site_folder, subfolder))
+
+def _get_latest_source(source_folder):
+    if exists(source_folder + '/.git'):
+        run('cd %s && git fetch' % (source_folder,))
     else:
-        run(f'git clone {REPO_URL} .')
-    current_commit = local("git log -n l --format=%H", capture=True)
-    run(f'git reset --hard {current_commit}')
+        run('git clone %s %s' % (REPO_URL, source_folder))
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
 
-def _update_virtualenv():
-    if not exists('virtualenv/bin/pip'):
-        run(f'python3.6 -m venv virtualenv')
-    run('./virtualenv/bin/pip install -r requirements.txt')
+def _update_setting(source_folder, site_name):
+    settings_path = source_folder + '/superlists/settings.py'
+    sed(settings_path, "DEBUG = True", "DEBUG = False")
+    sed(settings_path,'ALLOWED_HOSTS =.+$','ALLOWED_HOSTS = ["%s"]' % (site_name,))
+    secret_key_file = source_folder + '/superlists/secret_key.py'
+    if not exists(secret_key_file):
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
-def _create_or_update_dotenv():
-    append('.env', 'DJANGO_DEBUG_FALSE=y')
-    append('.env', f'SITENAME={env.host}')
-    current_contents = run('cat .env')
-    if 'DJANGO_SECRET_KEY' not in current_contents:
-        new_secret = ''.join(random.SystemRandom().choices(
-            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
-        ))
-        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+# def _update_virtualenv(source_folder):
+#     virtualenv_folder = source_folder + '/../virtualenv'
+#     if not exists(virtualenv_folder + '/bin/pip'):
+#         run('python3 -m venv %s' % (virtualenv_folder,))
+#     run('%s/bin/pip install -r %s/requirements.txt' % (virtualenv_folder, source_folder))
 
-def _update_static_files():
-    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+# def _update_static_files(source_folder):
+#     run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput' % (source_folder,))
 
-def _update_database():
-    run('./virtualenv/bin/python manage.py migrate --noinput')
+# def _update_database(source_folder):
+#     run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput' % (source_folder,))
