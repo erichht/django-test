d7ab97b0470a1c240edd34cc20eea8e645b99eec
erichht <erichht71@gmail.com>
2020-09-20 11:48:03 +1200

    fab function for create session(using .env vars) && fixed source .env vars issue

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,7 +1,8 @@
-import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from .server_tools import reset_database
+import os
 import time
 
 MAX_WAIT = 10
@@ -9,9 +10,10 @@ MAX_WAIT = 10
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
